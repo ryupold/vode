4c620bcbc3bfa6a88caefd77bcf05ff5499cea1a
Michael Scherbakow <ryupold@gmail.com>
2026-05-14 23:23:52 +0200

    test: onUnmount should be called for all nested nodes when parent is removed

--- test/tests-mount-unmount.ts
+++ test/tests-mount-unmount.ts
@@ -39,14 +39,14 @@ export default {
                 [ARTICLE,
                     {
                         onMount: (s: unknown, ele: HTMLElement) => {
-                            mounts.push("mount article");
+                            mounts.push("mount outer");
                         }
                     },
 
                     [P,
                         {
                             onMount: (s: unknown, ele: HTMLElement) => {
-                                mounts.push("mount p");
+                                mounts.push("mount inner");
                             }
                         },
                         "foo",
@@ -56,7 +56,7 @@ export default {
             ]
         );
 
-        expect(mounts).toEqual(["mount p", "mount article"]);
+        expect(mounts).toEqual(["mount inner", "mount outer"]);
     },
 
     "onUnmount(): called when node is removed from the DOM": () => {
@@ -83,5 +83,39 @@ export default {
         expect(unmounts).toEqual([]);
         patch({ showArticle: false });
         expect(unmounts).toEqual(["unmount article"]);
+    },
+
+    "onUnmount(): called for all child nodes that have registerd when parent node is removed from the DOM": () => {
+        const root = document.createElement("div");
+        const container = document.createElement("div");
+        root.appendChild(container);
+
+        const unmounts: string[] = [];
+
+        const state = createState({ showArticle: true });
+        const patch = app<typeof state>(container, state, (s) =>
+            [DIV,
+                s.showArticle && [ARTICLE,
+                    {
+                        onUnmount: (s: unknown, ele: HTMLElement) => {
+                            unmounts.push("unmount outer");
+                        }
+                    },
+                    [P,
+                        {
+                            onUnmount: (s: unknown, ele: HTMLElement) => {
+                                unmounts.push("unmount inner");
+                            }
+                        },
+                        "foo",
+                        [SPAN, "bar"]
+                    ],
+                ]
+            ]
+        );
+
+        expect(unmounts).toEqual([]);
+        patch({ showArticle: false });
+        expect(unmounts).toEqual(["unmount inner", "unmount outer"]);
     },
 }
